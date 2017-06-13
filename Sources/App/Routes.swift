7cb64ad81117dad4aeedb584ed37100a4a6ff605
Ivan Minier <ivanminier@me.com>
2017-06-13 16:40:45 -0700

    adding comments

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -3,17 +3,20 @@ import Vapor
 final class Routes: RouteCollection {
     func build(_ builder: RouteBuilder) throws {
         
-        builder.get("hello") { req in
+        builder.get("json") { req in
             
-            // JSON is a Structured Data wrapper for json with a 
+            // JSON is a StructuredDataWrapper with a
             // mutating function that takes multiple combinations of
-            // Lists with embeded dictionaries to be converted to JSON
+            // tuple or tuple dictionary
             
             var json = JSON()
-            try json.set("Name", "Bill")
+            try json.set("name", "Bill")
             try json.set("age", 30)
             try json.set("Homes", ["2334 Crescent Lake Dr." : "$400,000",
                                    "597 Elmwood Cir" : "298,000"])
+            
+            //try json.set("test", "list","test", "list","test", "list","test", "list","test", "list","test", "list","test", "list")
+            
             return json
         }
  
